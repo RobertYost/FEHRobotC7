7f93fc0ee91737e41ef739bc6dc06a8c3175397f
Bobby Yost <bobby.9202@gmail.com>
2017-02-22 13:48:04 -0500

    I don't know why I keep on having to commit this but hopefully it works this time

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
 #include <FEHLCD.h>
 #include <FEHIO.h>
-#include <FEHUtility.h>
+#include <FEHMotor.h>
+
+AnalogInputPin left(FEHIO::P1_2);
+AnalogInputPin middle(FEHIO::P1_1);
+AnalogInputPin right(FEHIO::P1_0);
+
+DigitalEncoder leftEncoder(FEHIO::P0_1);
+DigitalEncoder rightEncoder(FEHIO::P0_0);
+
+FEHMotor leftMotor( FEHMotor::Motor0, 12.0);
+FEHMotor rightMotor( FEHMotor::Motor1, 12.0);
+
+#define STRAIGHT_LEFT_THRESHOLD 1.543
+#define STRAIGHT_MIDDLE_THRESHOLD 1.7003
+#define STRAIGHT_RIGHT_THRESHOLD 2.097
+
+#define CURVED_LEFT_THRESHOLD 1.275
+#define CURVED_MIDDLE_THRESHOLD 1.4175
+#define CURVED_RIGHT_THRESHOLD 1.491
+
+#define CENTER 0
+#define RIGHT 1
+#define LEFT 2
+
+
+int determineState() {
+    int state = CENTER;
+    bool leftBool, centerBool, rightBool;
+    leftBool = left.Value() > CURVED_LEFT_THRESHOLD;
+    centerBool = middle.Value() > CURVED_MIDDLE_THRESHOLD;
+    rightBool = right.Value() > CURVED_RIGHT_THRESHOLD;
+
+    if (centerBool) {
+        state = CENTER;
+    } else {
+        if (leftBool) {
+            state = LEFT;
+        }
+        if (rightBool) {
+            state = RIGHT;
+        }
+    }
+
+    return state;
+}
+
+void reset () {
+    leftMotor.Stop();
+    rightMotor.Stop();
+    leftEncoder.ResetCounts();
+    rightEncoder.ResetCounts();
+}
+
+void Drive(int inches) {
+    reset();
+    leftMotor.SetPercent(25);
+    rightMotor.SetPercent(25);
+    while ((leftEncoder.Counts() + rightEncoder.Counts()) / 2 <= 200 * inches );
+}
 
 int main(void)
 {
@@ -8,15 +66,9 @@ int main(void)
     LCD.Clear( FEHLCD::Black );
     LCD.SetFontColor( FEHLCD::White );
 
-    float x,y;
-
     while( true )
     {
-        if( LCD.Touch(&x,&y) )
-        {
-            LCD.WriteLine( "Hello World!" );
-            Sleep( 100 );
-        }
+
     }
     return 0;
 }
